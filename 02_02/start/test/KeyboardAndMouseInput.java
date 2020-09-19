b43dfc7d8a29a1b391e6a698911a891dcf40e9c9
shraz36 <shraz36@gmail.com>
2020-09-19 20:36:55 +0100

    Intial commt

---
+++ 02_02/start/test/KeyboardAndMouseInput.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class KeyboardAndMouseInput {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/shraz/QCSE3/programs/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+
+        driver.quit();
+    }
+}
